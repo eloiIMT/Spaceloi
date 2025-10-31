a09860bfdf0acf88078036c09946cf09b65ad83c
Eloi Garnier <eloi.garnier@etu.univ-nantes.fr>
2025-10-31 12:06:03 +0100

    Ajout du service

---
+++ lib/ui/data/api/launch.service.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:spaceloi/data/models/launch.model.dart';
+import 'package:http/http.dart' as http;
+import 'package:spaceloi/data/models/rocket.model.dart';
+
+class LaunchService {
+
+  static const String baseUrl = 'https://api.spacexdata.com/v4';
+
+  static Future<List<Launch>> getLaunches() async {
+    final response = await http.get(Uri.parse('$baseUrl/launches'));
+    if (response.statusCode != 200) {
+      throw Exception('Failed to load launches');
+    }
+    final List<dynamic> launchesJson = json.decode(response.body);
+    return launchesJson.map((json) => Launch.fromJson(json)).toList();
+  }
+
+  static Future<Rocket> getRocketById(String id) async {
+    final response = await http.get(Uri.parse('$baseUrl/rockets/$id'));
+    if (response.statusCode != 200) {
+      throw Exception('Failed to load rocket');
+    }
+    final Map<String, dynamic> rocketJson = json.decode(response.body);
+    return Rocket.fromJson(rocketJson);
+  }
+}
